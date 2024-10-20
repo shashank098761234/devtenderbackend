93099c8e8c4bcfe5f1bba13b9baf17b96a947ff7
Shashank R <shashankr@Zeliots-MacBook-Air-2.local>
2024-10-20 17:06:32 +0530

    Feed api and pagination done

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 const userRouter = express.Router();
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
@@ -68,6 +69,51 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     });
   } catch (error) {
     res.status(400).send("Error : " + error.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    // User should see all the user card except
+    // 0. own card
+    // 1. his connections
+    // 2. ignored connections
+    // 3. already sent the connection request
+
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 2;
+    limit = limit > 2 ? 2 : limit;
+
+    const skip = (page - 1) * limit;
+
+    //Find all connection request that i have sent or received
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUserFromFeed = new Set();
+    connectionRequest.forEach((req) => {
+      hideUserFromFeed.add(req.fromUserId.toString());
+      hideUserFromFeed.add(req.toUserId.toString());
+    });
+
+    console.log(Array.from(hideUserFromFeed));
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserFromFeed) } }, //$nin =  not in this array
+        { _id: { $ne: loggedInUser._id } }, // $ne = not equal to
+      ],
+    })
+      .select("firstName lastName age gender about skills")
+      .skip(skip)
+      .limit(limit);
+
+    res.send(users);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
 });
 
